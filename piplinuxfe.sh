bddc6c9678da4c280f9c0bcf131c9fd1961c342b
LiveKnife <kirill26092014@gmail.com>
2024-06-12 07:28:36 +1000

    ElectroBrain

---
+++ piplinuxfe.sh
@@ -0,0 +1,36 @@
+echo "Fedora Python"
+sudo dnf install python3
+sudo dnf install python3-pip
+echo "Colorama"
+pip install colorama
+echo "Os"
+pip install os
+echo "Random"
+pip install random
+echo "Datetime"
+pip install datetime
+echo "Art"
+pip install art
+echo "Webbrowser"
+pip install webbrowser
+echo "Time"
+pip install time
+echo "Getpass"
+pip install getpass
+echo "Shutil"
+pip install shutil
+echo "G4F"
+pip install g4f
+echo "Search"
+pip install search
+echo "Pillow"
+pip install Pillow
+echo "Playsound"
+pip install playsound
+echo "Psutil"
+pip install psutil
+echo "FEDORA   TKINTER"
+sudo dnf install python3-tkinter
+
+pip install -U g4f
+echo "COMPLECTED"
